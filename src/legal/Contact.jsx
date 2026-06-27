58c106af63d499338e7d2e1777a467f253076506
JE Webmarketing <contact@je-webmarketing.com>
2026-06-27 21:17:26 +0200

    Add Contact legal page

---
+++ src/legal/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import LegalLayout from "./LegalLayout";
+
+export default function Contact() {
+  return (
+    <LegalLayout title="Contact">
+      <p><strong>JE-Webmarketing</strong></p>
+      <p>Eric JARRY</p>
+      <p>
+        494 rue Léon Blum<br />
+        34000 Montpellier<br />
+        France
+      </p>
+      <p>
+        Email : contact@je-webmarketing.com<br />
+        France : +33 6 70 02 46 41<br />
+        Suisse : +41 78 249 41 28
+      </p>
+    </LegalLayout>
+  );
+}
